2737c65ff608168c02a6e8f2bf71be594e9c04b5
Alex Branham <branham@utexas.edu>
2016-06-20 18:12:41 -0500

    Modifications

--- system-packages.el
+++ system-packages.el
@@ -1,9 +1,28 @@
+;;; system-packages.el --- functions to manage system packages
+
+;; Copyright
+
+;; Author: J. Alexander Branham
+;; Version: 0.1
+
+(defvar system-packages-packagemanager nil
+  "String containing the package manager to use")
+
 (when (executable-find "pacman")
   (setq system-packages-packagemanager "pacman"))
 (when (executable-find "apt-get")
   (setq system-packages-packagemanager "apt-get"))
 (when (executable-find "brew")
   (setq system-packages-packagemanager "brew"))
+
+(defvar system-packages-installcommand nil
+  "Defines the command to use to install system packages ")
+(defvar system-packages-searchcommand nil
+  "Defines the command to use to search for system packages")
+(defvar system-packages-uninstallcommand nil
+  "Defines the command to use to uninstall system packages")
+(defvar system-packages-updatecommand nil
+  "Defines the command to use to update system packages")
 
 (when (equal system-packages-packagemanager "pacman")
   (setq system-packages-installcommand "pacman -S"
@@ -23,16 +42,15 @@
         system-packages-uninstallcommand "brew uninstall"
         system-packages-updatecommand "brew update && brew upgrade"))
 
-(setq system-packages-usesudo t)
+(defvar system-packages-usesudo t
+  "If non-nil, system-packages will use sudo for appropriate commands")
 
 (defun system-packages-install ()
   (if (equal system-packages-usesudo t)
       (async-shell-command (concat "sudo " system-packages-installcommand)))
   (async-shell-command system-packages-installcommand))
 (defun system-packages-search ()
-  (if (equal system-packages-usesudo t)
-      (async-shell-command (concat "sudo " system-packages-searchcommand))
-    (async-shell-command system-packages-searchcommand)))
+  (async-shell-command system-packages-searchcommand))
 (defun system-packages-uninstall ()
   (if (equal system-packages-usesudo t)
       (async-shell-command (concat "sudo " system-packages-uninstallcommand)))B
