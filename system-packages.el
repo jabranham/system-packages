ba902ce6602649aefda675e3c3cfcf20ac7995f2
Alex Branham <branham@utexas.edu>
2018-01-31 18:24:33 -0600

    Mention helm-system-packages in commentary and README

--- system-packages.el
+++ system-packages.el
@@ -31,6 +31,9 @@
 ;; functions include installing packages, removing packages, listing
 ;; installed packages, and others.
 
+;; Helm users might also be interested in helm-system-packages.el
+;; <https://github.com/emacs-helm/helm-system-packages>
+
 ;; Usage:
 
 ;; (require 'system-packages)
