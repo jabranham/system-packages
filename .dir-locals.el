26feaab58211913d14707b953fbf8a23a48d2b95
Alex Branham <branham@utexas.edu>
2018-03-09 16:57:28 -0600

    Include bug reference info in .dir-locals

--- .dir-locals.el
+++ .dir-locals.el
@@ -1,3 +1,4 @@
 ((nil . ((fill-column . 70)
          (sentence-end-double-space . t)
-         )))
+         (bug-reference-bug-regexp . "#\\(?2:[0-9]+\\)")
+         (bug-reference-url-format . "https://github.com/jabranham/system-packages/issues/%s"))))
